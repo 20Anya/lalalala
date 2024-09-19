12628a47c75de9cb733205e1f9b2aa51e4aa5f13
Anya <aana45751@gmail.com>
2024-09-19 13:43:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ for file in files:
 with open('new.txt', 'w', encoding='utf-8') as w:
     for key in sorted(rewrite.keys()):
         w.write(rewrite[key])
+
+
+
+
+def process_files(directory, output_file):
+    files = os.listdir(directory)
+    rewrite = {}
+    for file in files:
+        with open(os.path.join(directory, file), encoding='utf-8') as f:
+            read_f = f.readlines()
+            count = len(read_f)
+            rewrite[count] = f"{file}\n{count}\n{'\\n'.join(read_f)}\n"
+    with open(output_file, 'w', encoding='utf-8') as w:
+        for key in sorted(rewrite.keys()):
+            w.write(rewrite[key])
+
+process_files('C:\\Users\\Пользователь\\Desktop\\lalalala\\txt', 'new.txt')
